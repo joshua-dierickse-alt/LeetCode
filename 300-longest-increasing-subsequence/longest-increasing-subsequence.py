f1d804f2c2a39619b2076c08471c6dbe1fb4df1e
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2025-09-07 11:58:55 -0400

    Time: 1609 ms (64.33%) | Memory: 18.2 MB (38.66%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -0,0 +1,16 @@
+def get_max(nums, dp, j):
+    m = 0
+    for i in range(j):
+        if nums[i] < nums[j]:
+            m = max(m, dp[i])
+    return m
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+
+        for i in range(len(nums)):
+            dp[i] = get_max(nums, dp, i) + 1
+
+        return max(dp)
+        
