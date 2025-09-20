724f336b5941d3ed2d2d8634cb5a22556fc151e8
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2025-09-20 14:36:36 -0400

    Time: 1203 ms (66.21%) | Memory: 18.6 MB (51.81%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

--- 3873-subsequence-sum-after-capping-elements/subsequence-sum-after-capping-elements.py
+++ 3873-subsequence-sum-after-capping-elements/subsequence-sum-after-capping-elements.py
@@ -3,25 +3,19 @@ class Solution:
         m = {}
 
         for i in range(len(nums)):
-            if nums[i] in m:
-                m[nums[i]] += 1
-            else:
-                m[nums[i]] = 1
+            m[nums[i]] = m.get(nums[i], 0) + 1
 
-        sorted_keys = []
-
-        for key in m.keys():
-            sorted_keys.append(key)
-
-        sorted_keys.sort()
+        sorted_keys = sorted(m.keys())
 
         dp = [None] * (k + 1)
-
         dp[0] = (0, 0)
 
-        for i in range(len(sorted_keys)):
-            for j in range(sorted_keys[i], len(dp)):
-                if dp[j - sorted_keys[i]] != None and dp[j] == None:
+        dp_len = len(dp)
+        sorted_key_len = len(sorted_keys)
+
+        for i in range(sorted_key_len):
+            for j in range(sorted_keys[i], dp_len):
+                if dp[j - sorted_keys[i]] is not None and dp[j] is None:
                     if dp[j - sorted_keys[i]][0] == sorted_keys[i]:
                         if dp[j - sorted_keys[i]][1] < m[sorted_keys[i]]:
                             dp[j] = (sorted_keys[i], dp[j - sorted_keys[i]][1] + 1)
@@ -31,15 +25,14 @@ class Solution:
         f = []
 
         num_x = 0
-        p = len(sorted_keys) - 1
-
+        p = sorted_key_len - 1
 
         for x in range(len(nums), 0, -1):
             found = False
             for i in range(num_x + 1):
-                if 0 > len(dp) - (i * x) - 1:
+                if 0 > dp_len - (i * x) - 1:
                     break
-                if dp[len(dp) - (i * x) - 1] != None and dp[len(dp) - (i * x) - 1][0] <= x:
+                if dp[dp_len - (i * x) - 1] is not None and dp[dp_len - (i * x) - 1][0] <= x:
                     found = True
                     break
             f.append(found)
