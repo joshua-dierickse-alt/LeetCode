af2bbdef9d75bfb640bf0296d8e1c8fdecaf1851
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2025-09-20 14:46:47 -0400

    Time: 1163 ms (66.21%) | Memory: 18.8 MB (33.86%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

--- 3873-subsequence-sum-after-capping-elements/subsequence-sum-after-capping-elements.py
+++ 3873-subsequence-sum-after-capping-elements/subsequence-sum-after-capping-elements.py
@@ -11,21 +11,23 @@ class Solution:
         dp[0] = (0, 0)
 
         dp_len = len(dp)
-        sorted_key_len = len(sorted_keys)
+        sorted_keys_len = len(sorted_keys)
 
-        for i in range(sorted_key_len):
-            for j in range(sorted_keys[i], dp_len):
-                if dp[j - sorted_keys[i]] is not None and dp[j] is None:
-                    if dp[j - sorted_keys[i]][0] == sorted_keys[i]:
-                        if dp[j - sorted_keys[i]][1] < m[sorted_keys[i]]:
-                            dp[j] = (sorted_keys[i], dp[j - sorted_keys[i]][1] + 1)
+        for i in range(sorted_keys_len):
+            key = sorted_keys[i]
+            for j in range(key, dp_len):
+                prev = dp[j - key]
+                if prev is not None and dp[j] is None:
+                    if prev[0] == key:
+                        if prev[1] < m[key]:
+                            dp[j] = (key, prev[1] + 1)
                     else:
-                        dp[j] = (sorted_keys[i], 1)
+                        dp[j] = (key, 1)
         
         f = []
 
         num_x = 0
-        p = sorted_key_len - 1
+        p = sorted_keys_len - 1
 
         for x in range(len(nums), 0, -1):
             found = False
