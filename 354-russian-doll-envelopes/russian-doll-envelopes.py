7ce2de095c2560f0cacdc3634b08ad69a5d2ae34
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2025-10-08 20:01:37 -0400

    Time: 155 ms (50.32%) | Memory: 52.8 MB (50.67%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -0,0 +1,34 @@
+from bisect import bisect_left
+
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort()
+
+        dp = []
+
+        i = 0
+
+        while i < len(envelopes):
+            prev_x = envelopes[i][0]
+            j = 0
+            last_index = -1
+            updates = []
+
+            while i + j < len(envelopes) and prev_x == envelopes[i + j][0]:
+                update_index = bisect_left(dp, envelopes[i + j][1])
+                if update_index != last_index:
+                    last_index = update_index
+                    updates.append((update_index, envelopes[i + j][1]))
+
+                j += 1
+
+            for [update_index, value] in updates:
+                if update_index >= len(dp):
+                    dp.append(value)
+                else:
+                    dp[update_index] = value
+
+            i += j
+            
+        return len(dp)
+            
