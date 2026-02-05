c197cf396c78bad0e55c73cc718e5a39e0a9f5f6
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-02-05 17:16:20 -0500

    Time: 2145 ms (17.83%) | Memory: 75.2 MB (30.82%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

---
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        # M[i][m][n] -> given the first i elements, m 0's, and n 1's, this is the max # of i elements we can fit
+
+        def count_01(s):
+            zeros = 0
+            ones = 0
+            for c in s:
+                if c == "0":
+                    zeros += 1
+                else:
+                    ones += 1
+            return (zeros, ones)
+
+        dp = [[[0] * (n + 1) for _ in range(m + 1)] for _ in range(len(strs) + 1)]
+
+        for i in range(1, len(strs) + 1):
+            
+            zeros, ones = count_01(strs[i - 1])
+
+            for m_i in range(m + 1):
+                for n_i in range(n + 1):
+                    if m_i - zeros >= 0 and n_i - ones >= 0:
+                        dp[i][m_i][n_i] = max(dp[i - 1][m_i][n_i], 1 + dp[i - 1][m_i - zeros][n_i - ones])
+                    else:
+                        dp[i][m_i][n_i] = dp[i - 1][m_i][n_i]
+
+        return dp[-1][-1][-1]
+        
