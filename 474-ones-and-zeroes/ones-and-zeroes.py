2686d09460d2691e0076b1f67c526d4665d9783b
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-02-05 17:33:09 -0500

    Time: 947 ms (78.31%) | Memory: 19.6 MB (56.59%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

--- 474-ones-and-zeroes/ones-and-zeroes.py
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -12,18 +12,14 @@ class Solution:
                     ones += 1
             return (zeros, ones)
 
-        dp = [[[0] * (n + 1) for _ in range(m + 1)] for _ in range(len(strs) + 1)]
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
         for i in range(1, len(strs) + 1):
-            
             zeros, ones = count_01(strs[i - 1])
 
-            for m_i in range(m + 1):
-                for n_i in range(n + 1):
-                    if m_i - zeros >= 0 and n_i - ones >= 0:
-                        dp[i][m_i][n_i] = max(dp[i - 1][m_i][n_i], 1 + dp[i - 1][m_i - zeros][n_i - ones])
-                    else:
-                        dp[i][m_i][n_i] = dp[i - 1][m_i][n_i]
+            for m_i in range(m, zeros - 1, -1):
+                for n_i in range(n, ones - 1, -1):
+                    dp[m_i][n_i] = max(dp[m_i][n_i], 1 + dp[m_i - zeros][n_i - ones])
 
-        return dp[-1][-1][-1]
+        return dp[-1][-1]
         
