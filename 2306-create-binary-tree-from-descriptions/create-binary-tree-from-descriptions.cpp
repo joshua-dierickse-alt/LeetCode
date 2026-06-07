805e052f8f18162a4f5ccfb8d09f089e0290d21c
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-06-07 17:24:40 -0400

    Time: 127 ms (90.58%) | Memory: 259.4 MB (88.87%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -13,36 +13,28 @@ class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
         unordered_map<int, TreeNode*> m;
-
-        unordered_set<int> s;
-        for (const auto &d : descriptions) {
-            s.insert(d[0]);
-            s.insert(d[1]);
-        }
+        int root = 0;
 
         for (const auto &d : descriptions) {
             int p = d[0];
             int c = d[1];
             int is_left = d[2];
 
-            s.erase(c);
-
-            if (!m.contains(p) && !m.contains(c)) {
-                m[c] = new TreeNode(c);
-                m[p] = new TreeNode(p, is_left ? m[c] : nullptr, is_left ? nullptr : m[c]);
-            } else if (!m.contains(c)) {
-                TreeNode* &choice = is_left ? m[p]->left : m[p]->right;
-                choice = new TreeNode(c);
-                m[c] = choice;
-            } else if (!m.contains(p)) {
-                m[p] = new TreeNode(p, is_left ? m[c] : nullptr, is_left ? nullptr : m[c]);
-            } else {
-                TreeNode* &choice = is_left ? m[p]->left : m[p]->right;
-                choice = m[c];
+            if (!m.contains(p)) { 
+                m[p] = new TreeNode(p);
+                root ^= p;
             }
+            if (!m.contains(c)) {
+                m[c] = new TreeNode(c);
+                root ^= c;
+            }
+
+            if (is_left) m[p]->left = m[c];
+            else m[p]->right = m[c];
+            
+            root ^= c;
         }
 
-        int head = *s.begin();
-        return m[head];
+        return m[root];
     }
 };
