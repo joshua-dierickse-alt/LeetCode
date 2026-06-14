a5ec28f20210b03dcdb5b697f80c66cde09ca6ca
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-06-14 12:11:45 -0400

    Time: 39 ms (92.49%) | Memory: 180.4 MB (75.49%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

---
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -0,0 +1,46 @@
+class NumArray {
+public:
+    vector<int> nums;
+    vector<int> fenwick;
+
+    NumArray(vector<int>& n) : nums(n), fenwick(n.size(), 0) {
+        for (size_t i = 1; i <= nums.size(); ++i) {
+            int k = 1;
+            while (i % k == 0) k <<= 1;
+            k >>= 1;
+            int s = 0;
+            for (int j = 0; j < k; ++j) s += nums[(i - 1) - j];
+            fenwick[i - 1] = s;
+        }
+    }
+    
+    void update(int index, int val) {
+        int diff = val - nums[index];
+
+        int i = index + 1;
+        while (i <= nums.size()) {
+            fenwick[i - 1] += diff;
+            i += i & -i;
+        }
+
+        nums[index] = val;
+    }
+
+    int sumRange(int r) {
+        if (r == -1) return 0;
+
+        int s = 0;
+        r += 1;
+
+        while (r >= 1) {
+            s += fenwick[r - 1];
+            r -= r & -r;
+        }
+
+        return s;
+    }
+    
+    int sumRange(int left, int right) {
+        return sumRange(right) - sumRange(left - 1);
+    }
+};
