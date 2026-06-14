781c2580ed1d46ded7b7422f2784a1eecc1657de
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-06-14 12:29:57 -0400

    Time: 19 ms (99.93%) | Memory: 181.3 MB (60.52%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -3,14 +3,17 @@ public:
     vector<int> nums;
     vector<int> fenwick;
 
-    NumArray(vector<int>& n) : nums(n), fenwick(n.size(), 0) {
-        for (size_t i = 1; i <= nums.size(); ++i) {
-            int k = 1;
-            while (i % k == 0) k <<= 1;
-            k >>= 1;
-            int s = 0;
-            for (int j = 0; j < k; ++j) s += nums[(i - 1) - j];
-            fenwick[i - 1] = s;
+    NumArray(vector<int>& n) : nums(n), fenwick(n.size()) {
+        int s = 0;
+        vector<int> prefix_sum(nums.size() + 1);
+        for (int i = 0; i < nums.size(); ++i) {
+            s += nums[i];
+            prefix_sum[i + 1] = s;
+        }
+
+        for (int i = 1; i <= nums.size(); ++i) {
+            int k = i & -i;
+            fenwick[i - 1] = prefix_sum[i] - prefix_sum[i - k];
         }
     }
     
