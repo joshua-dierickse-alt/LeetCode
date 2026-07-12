b53afdf58b29d6e9444fdd18a75979c553e0c161
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-07-12 10:55:44 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.78%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -3,11 +3,5 @@ class Solution:
         if num == 0:
             return 0
 
-        c = num.bit_length() - 1
-
-        while num > 0:
-            num -= num & -num
-            c += 1
-
-        return c  
+        return num.bit_length() - 1 + num.bit_count()
 
