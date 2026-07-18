51eb7c031f4538d83652143f671051ffa59613cd
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-07-18 12:00:35 -0700

    Time: 9 ms (80.89%) | Memory: 34.3 MB (46.75%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

--- 816-design-hashset/design-hashset.c
+++ 816-design-hashset/design-hashset.c
@@ -1,22 +1,24 @@
+long BINARY_1S = 0b1111111111111111111111111111111111111111111111111111111111111111;
+
 typedef struct {
-    bool arr[1000001];
+    unsigned long arr[1000001 / 64 + 1];
 } MyHashSet;
 
 
 MyHashSet* myHashSetCreate() {
-    return (MyHashSet*) calloc(1, sizeof(MyHashSet));
+    return (MyHashSet*)calloc(1, sizeof(MyHashSet));
 }
 
 void myHashSetAdd(MyHashSet* obj, int key) {
-    obj->arr[key] = true;
+    obj->arr[key / 64] |= 1UL << (key % 64);
 }
 
 void myHashSetRemove(MyHashSet* obj, int key) {
-    obj->arr[key] = false;
+    obj->arr[key / 64] &= BINARY_1S - (1UL << (key % 64));
 }
 
 bool myHashSetContains(MyHashSet* obj, int key) {
-    return obj->arr[key];
+    return (obj->arr[key / 64] & (1UL << (key % 64))) > 0;
 }
 
 void myHashSetFree(MyHashSet* obj) {
