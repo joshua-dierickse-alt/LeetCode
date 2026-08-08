402f23e5aa273819eeadd77d79ff2e5e484b27b3
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-08-08 12:15:50 -0700

    Time: 251 ms (92.54%) | Memory: 40.6 MB (92.88%) - LeetSync
    Co-authored-by: Joshua Dierickse <joshua.dierickse@gmail.com>

---
+++ 1710-find-servers-that-handled-most-number-of-requests/find-servers-that-handled-most-number-of-requests.py
@@ -0,0 +1,35 @@
+import heapq
+
+class Solution:
+    def busiestServers(self, k: int, arrivals: List[int], loads: List[int]) -> List[int]:
+        requests_handled = [0] * k
+        
+        available_servers = [i for i in range(k)]
+
+        unavailable_servers = []
+
+        for i in range(len(arrivals)):
+            arrival = arrivals[i]
+            load = loads[i]
+
+            while unavailable_servers and unavailable_servers[0][0] <= arrival:
+                _, server = heapq.heappop(unavailable_servers)
+                server = server % k
+                
+                if i % k <= server:
+                    generation = (i // k) * k + server
+                else:
+                    generation = (i // k + 1) * k + server
+                    
+                heapq.heappush(available_servers, generation)
+
+            if not available_servers:
+                continue
+
+            server = heapq.heappop(available_servers) % k
+            heapq.heappush(unavailable_servers, (arrival + load, server))
+            requests_handled[server] += 1
+
+        max_handled = max(requests_handled)    
+        
+        return [i for i in range(k) if requests_handled[i] == max_handled]
