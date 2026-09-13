3cf6a0587b3bacf0a7ac5f0d13da8ad1949c1e7a
Joshua Dierickse <joshua.spamemails.22@gmail.com>
2026-09-13 02:58:49 -0700

    Time: 89 ms (5.45%) | Memory: 29.7 MB (14.65%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        L = height[l]
+        R = height[r]
+
+        result = 0
+
+        def update():
+            nonlocal result
+            result = max(result, (r - l) * min(L, R))
+
+        while l < r:
+            update()
+            if L < R:
+                l += 1
+                L = max(L, height[l])
+            else:
+                r -= 1
+                R = max(R, height[r])
+
+        return result
